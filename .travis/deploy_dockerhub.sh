7e02aa197732eeaebd544a5d73b51d07b70d7de9
Jayden Liang <jaydenliang81@gmail.com>
2018-06-17 00:44:22 -0700

    fixing bugs for calling deploy_dockerhub.sh

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
-docker login -u $DOCKER_USER --password-stdin <<< $DOCKER_PASS
-if ["$TRAVIS_BRANCH" = "master"] then
+#!/bin/sh
+docker login -u $DOCKER_USER --password $DOCKER_PASS
+if [ "$TRAVIS_BRANCH" = "master" ]; then
   TAG="latest"
 else
   TAG="$TRAVIS_BRANCH"
@@ -9,4 +9,4 @@ fi
 #docker build python
 DOCKER_REPO="encors/encors"
 docker build -f python/Dockerfile -t $DOCKER_REPO-py:$TAG python/
-docker push $DOCKER_REPO-py
+#docker push $DOCKER_REPO-py
