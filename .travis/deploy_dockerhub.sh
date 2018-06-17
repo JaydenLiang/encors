b38ab30646710c6a0093a423ba491ca511f15f08
Jayden Liang <jaydenliang81@gmail.com>
2018-06-17 01:31:23 -0700

    testing on calling deploy_dockerhub.sh

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 docker login -u $DOCKER_USER --password $DOCKER_PASS
 if [ "$1" = "staging" ]; then
-  TAG="staging"
+    TAG="staging"
 elif [ "$1" = "production" ]; then
-  TAG="latest"
+    TAG="latest"
+else
+    TAG="test"
 fi
 
 #docker build python
-DOCKER_REPO="encors/encors"
-docker build -f python/Dockerfile -t $DOCKER_REPO-py:$TAG python/
-#docker push $DOCKER_REPO-py
+DOCKER_REPO="encors/encors-py"
+docker build -f python/Dockerfile -t $DOCKER_REPO:$TAG python/
+#push to docker hub
+if [ "$2" = "push" ]; then
+    docker push $DOCKER_REPO
